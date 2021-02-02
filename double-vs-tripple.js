81ada003f487095a20ca979aed662d32491e38e4
RKDATTA <rubelkdctg@gmail.com>
2021-02-02 12:39:19 +0600

    Onek kich shikhlam

---
+++ double-vs-tripple.js
@@ -0,0 +1,10 @@
+
+let first = 2;
+let second = "2";
+
+if(first === second){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
---
+++ true-false.js
@@ -0,0 +1,28 @@
+
+
+//Falsy
+//0
+//NaN
+//false
+// ""
+//null
+//undefined
+
+//Truthy
+// '0' " " []
+const age = 0;
+
+if (age){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+const name = "Solaiman"
+if (name.length > 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
